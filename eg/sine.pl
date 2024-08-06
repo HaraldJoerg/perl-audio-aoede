b3e51d1253a3b912010756c85a9e40a6fadb9fc5
Harald Jörg <haj@posteo.de>
2024-08-06 21:36:15 +0200

    A wave as a closure works just fine!

--- eg/sine.pl
+++ eg/sine.pl
@@ -6,17 +6,15 @@ use Feature::Compat::Class;
 use feature 'signatures';
 no warnings 'experimental';
 
-class Audio::Aoede::Voice::Sine {
-    use PDL;
-    use constant pi => atan2(0,-1); # Math::Trig collides with PDL
+use PDL;
+use constant pi => atan2(0,-1); # Math::Trig collides with PDL
 
-    use Audio::Aoede;
+use Audio::Aoede;
 
-    field $frequency :param;
-    field $samples_per_period = Audio::Aoede->rate / $frequency;
-    field $norm = 2 * pi * $frequency / Audio::Aoede->rate;
-
-    method next_samples ($n_samples,$since=0) {
+sub sine_wave ($frequency) {
+    my $samples_per_period = Audio::Aoede->rate / $frequency;
+    my $norm = 2 * pi / $samples_per_period;
+    return sub ($n_samples, $since = 0) {
         $since -= int ($since/$samples_per_period);
         my $phase = (sequence($n_samples) + $since) * $norm;
         my $samples = sin($phase);
@@ -26,10 +24,10 @@ class Audio::Aoede::Voice::Sine {
 
 use PDL;
 use Audio::Aoede::LPCM;
+my $wave = sine_wave(440);
+my $samples = $wave->(Audio::Aoede->rate);
 my $lpcm = Audio::Aoede::LPCM->new(
     rate => Audio::Aoede->rate,
-    data => short(Audio::Aoede::Voice::Sine->new(frequency =>440)
-                  ->next_samples(Audio::Aoede->rate) * 2**14)
-    ->get_dataref->$*
+    data => short($samples * 2**14)->get_dataref->$*,
     );
 $lpcm->write_wav('/tmp/sine.wav');
