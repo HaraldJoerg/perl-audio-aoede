5791cb701e28c2a14d1e0f078f07a8bd0ba770f1
Harald Jörg <haj@posteo.de>
2024-08-07 00:04:08 +0200

    Another variation: Delegating stuff to the Audio::Aoede package

--- eg/sine.pl
+++ eg/sine.pl
@@ -6,28 +6,10 @@ use Feature::Compat::Class;
 use feature 'signatures';
 no warnings 'experimental';
 
-use PDL;
-use constant pi => atan2(0,-1); # Math::Trig collides with PDL
 
-use Audio::Aoede;
+use Audio::Aoede qw( sine_wave );
 
-sub sine_wave ($frequency) {
-    my $samples_per_period = Audio::Aoede->rate / $frequency;
-    my $norm = 2 * pi / $samples_per_period;
-    return sub ($n_samples, $since = 0) {
-        $since -= int ($since/$samples_per_period);
-        my $phase = (sequence($n_samples) + $since) * $norm;
-        my $samples = sin($phase);
-        return $samples;
-    }
-}
-
-use PDL;
 use Audio::Aoede::LPCM;
 my $wave = sine_wave(440);
 my $samples = $wave->(Audio::Aoede->rate);
-my $lpcm = Audio::Aoede::LPCM->new(
-    rate => Audio::Aoede->rate,
-    data => short($samples * 2**14)->get_dataref->$*,
-    );
-$lpcm->write_wav('/tmp/sine.wav');
+Audio::Aoede::write_wav($samples);
--- lib/Audio/Aoede.pm
+++ lib/Audio/Aoede.pm
@@ -1,8 +1,36 @@
 # ABSTRACT: Create and Analyze Sound
-use 5.032;
 package Audio::Aoede 0.01;
+use 5.032;
+use feature 'signatures';
+no warnings 'experimental';
+
+use Exporter 'import';
+our @EXPORT_OK = qw( sine_wave );
 
 our $rate = 44100;
+use PDL;
+use constant pi => atan2(0,-1); # Math::Trig collides with PDL
 
 sub rate { return $rate }
+
+sub sine_wave ($frequency) {
+    my $samples_per_period = Audio::Aoede->rate / $frequency;
+    my $norm = 2 * pi / $samples_per_period;
+    return sub ($n_samples, $since = 0) {
+        $since -= int ($since/$samples_per_period);
+        my $phase = (sequence($n_samples) + $since) * $norm;
+        my $samples = sin($phase);
+        return $samples;
+    }
+}
+
+sub write_wav ($samples) {
+    use Audio::Aoede::LPCM;
+    my $lpcm = Audio::Aoede::LPCM->new(
+    rate => $rate,
+    data => short($samples * 2**14)->get_dataref->$*,
+    );
+    $lpcm->write_wav('/tmp/sine.wav');
+}
+
 1;
