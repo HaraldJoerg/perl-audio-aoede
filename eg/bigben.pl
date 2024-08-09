5b52582c1d375ca5e4554cc0c6740fa50a9ed159
Harald Jörg <haj@posteo.de>
2024-08-09 20:11:02 +0200

    Cleanup: Do Big Ben only once

--- eg/bigben.pl
+++ eg/bigben.pl
@@ -15,6 +15,10 @@ my $bigben = <<END_OF_SCORE;
 A3:𝅘𝅥  C♯4:𝅘𝅥 B3:𝅘𝅥  E3:𝅗𝅥  A3:𝅘𝅥  B3:𝅘𝅥  C♯4:𝅘𝅥 A3:𝅗𝅥
 C♯4:𝅘𝅥 A3:𝅘𝅥  B3:𝅘𝅥  E3:𝅗𝅥  E3:𝅘𝅥  B3:𝅘𝅥  C♯4:𝅘𝅥 A3:𝅗𝅥
 END_OF_SCORE
+# The same score in pure ASCII:
+# A2:1/4  C#3:1/4 B2:1/4  E2:1/2 A2:1/4  B2:1/4  C#3:1/4 A2:1/2
+# C#3:1/4 A2:1/4  B2:1/4  E2:1/2 E2:1/4  B2:1/4  C#3:1/4 A2:1/2
+
 
 my $voice = Audio::Aoede::Voice->new(function => sine_wave());
 for my $note (split /\s+/, $bigben) {
@@ -23,20 +27,3 @@ for my $note (split /\s+/, $bigben) {
 }
 $muse->write($voice);
 
-my $rest = Audio::Aoede::Voice->new(function => sine_wave());
-$rest->add_named_note('𝄼');
-$muse->write($rest);
-
-my $bigben_ascii = <<END_OF_SCORE;
-A2:1/4  C#3:1/4 B2:1/4  E2:1/2
-A2:1/4  B2:1/4  C#3:1/4 A2:1/2
-C#3:1/4 A2:1/4  B2:1/4  E2:1/2
-E2:1/4  B2:1/4  C#3:1/4 A2:1/2
-END_OF_SCORE
-
-$voice = Audio::Aoede::Voice->new(function => sine_wave());
-for my $note (split /\s+/, $bigben_ascii) {
-    next unless $note;
-    $voice->add_named_note($note);
-}
-$muse->write($voice);
