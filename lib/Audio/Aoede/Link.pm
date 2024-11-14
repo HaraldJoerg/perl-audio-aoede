1c36fa719eb49c7eec5509f74d215c3be7929f12
Harald Jörg <haj@posteo.de>
2024-11-14 18:07:39 +0100

    Cleanup: Use the :reader attribute in ::Link

--- lib/Audio/Aoede/Link.pm
+++ lib/Audio/Aoede/Link.pm
@@ -7,18 +7,8 @@ use feature 'signatures';
 no warnings 'experimental';
 
 class Audio::Aoede::Link {
-    field $offset :param = 0;
-    field $next          = $offset;
-
-
-    method next {
-        return $next;
-    }
-
-
-    method set_next ($new) {
-        $next = $new;
-    }
+    field $offset :param :reader = 0;
+    field $next          :reader = $offset;
 
 
     method done ($n_samples) {
@@ -28,11 +18,6 @@ class Audio::Aoede::Link {
 
     method done_to ($new_next) {
         $next = $new_next;
-    }
-
-
-    method offset {
-        return $offset;
     }
 }
 
