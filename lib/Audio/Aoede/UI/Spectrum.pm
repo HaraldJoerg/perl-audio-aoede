2c9af02aa1582b8c9d35e73175e7eff258cf9364
Harald Jörg <haj@posteo.de>
2025-01-16 20:36:04 +0100

    Feature: bin/spectre - add some UI
    * bin/spectre: Add buttons Play / Pause / Stop and a slider
    indicating the curent position
    (process_file): Add an "open file" dialog instead of a command
    line option
    
    * lib/Audio/Aoede/UI/Spectrum.pm: Replace the line spectrum by
    ppair::Spikes, show left/right intensity in blue/red and
    above/below the x axis

--- lib/Audio/Aoede/UI/Spectrum.pm
+++ lib/Audio/Aoede/UI/Spectrum.pm
@@ -6,14 +6,14 @@ no warnings 'experimental';
 
 class Audio::Aoede::UI::Spectrum;
 
-use Prima;
+use Prima qw( );
 use PDL;
 use PDL::Graphics::Prima;
 
 field $parent :param;
 field $size :param = [400,400];
 field $pack :param;
-field $rate :param;
+field $limit :param;
 field $plot;
 field $max_y = '-Inf';
 field %spectra;
@@ -37,6 +37,11 @@ my %colors = (
     right => cl::Red,
 );
 
+my %orientations = (
+    left => 1,
+    right => -1,
+);
+
 
 method update (%data) {
     my $n_samples = (values %data)[0]->dim(0);
@@ -55,15 +60,26 @@ method update (%data) {
         $max_y *= 0.99;
     }
     $plot->y->max($max_y);
+    $plot->y->min(-$max_y);
+    my $orientation = 1;
+    my $line_width = int ($plot->width / $n_samples + 0.9);
     for my ($channel,$spectrum) (%data) {
+        $orientation = - $orientation;
         $plot->dataSets->{$channel}  =
-        ds::Pair((sequence($n_samples) / $n_samples) * ($rate / 2),
-                 $spectrum,
-                 plotType => ppair::Lines,
-                 color => $colors{$channel} || cl::Black,
+            ds::Pair((sequence($n_samples) / $n_samples) * $limit,
+                     $spectrum * $orientations{$channel},
+                     plotType => ppair::Spikes(lineWidths => $line_width),
+                     color => $colors{$channel} || cl::Black,
              );
     }
 
     $plot->repaint;
 }
+
+
+method clear {
+    %{$plot->dataSets} = ();
+}
+
+
 1;
